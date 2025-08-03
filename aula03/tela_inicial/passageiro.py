5107d92fdaa739410c08a4cd17830625239301b8
Rodrigo Henrich <rodrigohenrich@faccat.br>
2025-08-03 11:45:17 -0300

    Tela para verificar status passageiro

---
+++ aula03/tela_inicial/passageiro.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+@st.cache_data
+def carrega_modelo():
+  return joblib.load('modelo_titanic_logistic_regression.pkl')
+
+st.set_page_config(page_title='Titanic Logistic Regression', page_icon='🚢')
+
+st.title('Informações do passageiro')
+idade = st.number_input('Qual a idade do passageiro', min_value=0, max_value=100)
+sexo_selecionado = st.selectbox('Qual o sexo do passageiro',['Feminino', 'Masculino'])
+sexo = 1 if sexo_selecionado=='Feminino' else 0
+classe_selecionada = st.selectbox('Selecione a classe do passageiro',['Primeira','Segunda', 'Terceira'])
+mapa_classe = {'Primeira': 1, 'Segunda': 2, 'Terceira': 3}
+classe = mapa_classe[classe_selecionada]
+numero_irmaos = st.number_input('Quantos irmãos ou cônjuges tinha a bordo', min_value=0)
+numero_filhos = st.number_input('Quantos pais ou filhos tinha a bordo', min_value=0)
+
+if st.button('Analisar'):
+  novo_passageiro = pd.DataFrame([{
+    'age': idade,
+    'sex': sexo,
+    'pclass': classe,
+    'parch': numero_irmaos,
+    'sibsp': numero_filhos
+  }])
+
+  st.dataframe(novo_passageiro)
+
+  lr = carrega_modelo()
+
+  predicao = lr.predict(novo_passageiro)
+
+  st.write('Sobreviveu' if predicao[0]==1 else 'Não Sobreviveu')
